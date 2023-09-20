347ab13ffeafddd6d6cf4c20de40fd3d81ae123a
Saviour Eking <saviour.eking@outlook.com>
2023-09-20 22:41:42 +0000

    SOLVED: fix game logic

--- starter/class/connect-four.js
+++ starter/class/connect-four.js
@@ -1,18 +1,29 @@
 const Screen = require("./screen");
 const Cursor = require("./cursor");
 
+const { isEmptyGrid,
+  isHorizontalWin,
+  isVerticalWin,
+  isTie,
+  isFullBoard,
+  isDiagonalWinUpwardWin,
+  isDiagonalWinDownwardWin
+} = require('../../_helper');
+
 class ConnectFour {
 
   constructor() {
 
     this.playerTurn = "O";
 
-    this.grid = [[' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ']]
+    this.grid = [
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+                ]
 
     this.cursor = new Cursor(6, 7);
 
@@ -22,15 +33,29 @@ class ConnectFour {
 
     // Replace this with real commands
     Screen.addCommand('t', 'test command (remove)', ConnectFour.testCommand);
+    Screen.addCommand('left', 'move left', this.cursor.left.bind(this.cursor));
+    Screen.addCommand('right', 'move right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('up', 'move up', this.cursor.up.bind(this.cursor));
+    Screen.addCommand('down', 'move down', this.cursor.down.bind(this.cursor));
 
+    Screen.addCommand('space', 'place move', this.placeMove.bind(this))
     this.cursor.setBackgroundColor();
     Screen.render();
   }
 
-  // Remove this
-  static testCommand() {
-    console.log("TEST COMMAND");
+  placeMove() {
+    Screen.render();
+    Screen.setGrid(this.cursor.row, this.cursor.col, this.playerTurn);
+    this.playerTurn === "O" ? this.playerTurn = "X" : this.playerTurn = "O";
+    const winner = ConnectFour.checkWin(Screen.grid);
+
+    !winner ? Screen.render() : ConnectFour.endGame(winner);
   }
+
+  // Remove this
+  // static testCommand() {
+  //   console.log("TEST COMMAND");
+  // }
 
   static checkWin(grid) {
 
@@ -38,6 +63,15 @@ class ConnectFour {
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
+
+    if (isFullBoard(grid)) {
+      return isTie(grid);
+    } else if (isEmptyGrid(grid)) {
+      return false;
+    }
+    else {
+      return isHorizontalWin(grid) || isVerticalWin(grid) || isDiagonalWinUpwardWin(grid) || isDiagonalWinDownwardWin(grid);
+    }
 
   }
 
