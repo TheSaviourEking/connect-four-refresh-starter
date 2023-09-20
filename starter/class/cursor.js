7e3137fcea08aa5d0086ea45a10ed061d1a88d86
Saviour Eking <saviour.eking@outlook.com>
2023-09-20 22:41:54 +0000

    SOLVED: fix game logic

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -6,12 +6,11 @@ class Cursor {
     this.numRows = numRows;
     this.numCols = numCols;
 
-    this.row = 0;
-    this.col = 0;
+    this.row = 0; // 0
+    this.col = 0; // 0
 
     this.gridColor = 'black';
     this.cursorColor = 'yellow';
-
   }
 
   resetBackgroundColor() {
@@ -22,15 +21,45 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
-  left() {
-    // Move cursor left
+  #movementHelper = (directions) => {
+    Screen.render();
+    this.resetBackgroundColor();
+    directions == "up" ? this.row--
+      : directions == "down" ? this.row++
+        : directions == "left" ? this.col--
+          : this.col++;
+    this.setBackgroundColor();
+    Screen.render();
   }
 
-  right() {
+  up = () => {
+    // Move cursor up
+    if (this.row > 0) {
+      this.#movementHelper('up')
+    }
+  }
+
+  down = () => {
+    // Move cursor down
+    if (this.row < this.numRows - 1) {
+      this.#movementHelper('down')
+    }
+  }
+
+  left = () => {
+    // Move cursor left
+    if (this.col > 0) {
+      this.#movementHelper('left')
+    }
+  }
+
+  right = () => {
     // Move cursor right
+    if (this.col < this.numCols - 1) {
+      this.#movementHelper('right');
+    }
   }
 
 }
-
 
 module.exports = Cursor;
